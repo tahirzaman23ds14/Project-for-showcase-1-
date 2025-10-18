6d85774e0f670e60bdf28d4c58173d42768f77f5
tahirzaman23ds14 <tahirzamanoffice1@gmail.com>
2025-10-18 13:57:20 +0500

    update

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ mobileLinks.forEach(link => {
     mobileMenu.classList.remove("show");
   });
 });
+const roles = ["Problem Solver", "Code Explorer", "AI Expert"];
+let index = 0;
+let charIndex = 0;
+const animatedText = document.getElementById("animated-text");
+
+function typeEffect() {
+  if (charIndex < roles[index].length) {
+    animatedText.textContent += roles[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeEffect, 120);
+  } else {
+    setTimeout(eraseEffect, 1200);
+  }
+}
+
+function eraseEffect() {
+  if (charIndex > 0) {
+    animatedText.textContent = roles[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, 70);
+  } else {
+    index = (index + 1) % roles.length;
+    setTimeout(typeEffect, 400);
+  }
+}
+
+typeEffect();
